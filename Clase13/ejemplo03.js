ff290aceb231e9708717df8dfbddb5985bd79a39
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-10-05 12:07:02 -0300

    ejemplo03

---
+++ Clase13/ejemplo03.js
@@ -0,0 +1,6 @@
+const nombre= prompt('Ingrese su nombre', ''); //cada vez que queremos ingresar un dati podemos usar esta finción, apararece una ventana y cargamos el valor.// el dato ingresado es un string o cadena de texto
+const edad = prompt('Ingrese su edad', '');
+
+document.write(`Hola mi nombre es ${nombre}, asi que tienes ${edad}`);
+
+console.log(`Hola mi nombre es ${nombre}, asi que tienes ${edad}`);
