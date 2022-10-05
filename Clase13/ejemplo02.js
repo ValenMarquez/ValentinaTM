1f663b164e7ec20fb63763487580455cfd8551b3
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-10-05 12:06:38 -0300

    ejemplo02

---
+++ Clase13/ejemplo02.js
@@ -0,0 +1,8 @@
+const nombre = 'Valentina';
+        const edad = 40;
+     
+
+        document.write('hola soy '  + nombre + '<br/>'); 
+        document.write(`Hola soy ${nombre}. Mi edad es ${edad} `); //interpolación de string
+        
+        console.log(nombre);
