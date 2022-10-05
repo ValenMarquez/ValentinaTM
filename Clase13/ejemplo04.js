b6d6111ac7be3e17dd00bcd11f653b977f0b4b56
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-10-05 12:07:16 -0300

    ejemplo04

---
+++ Clase13/ejemplo04.js
@@ -0,0 +1,12 @@
+const valor1 = prompt('Ingrese si primer numero');
+const valor2 = prompt('Ingrese si segundo numero');
+
+const suma = parseInt(valor1) + parseInt(valor2); // 7  ---- 52
+
+const producto = valor1 * valor2;
+//parseInt > funcion que el dato lo convierte  en un numero entero
+
+console.log(suma)  // 7     52
+console.log(producto)
+
+//const valor1 = (prompt('Ingrese su primer numero', ''));
