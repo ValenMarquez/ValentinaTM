0a14f169db2113a0a95c17e409c963f40b8e7c60
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-10-05 12:07:28 -0300

    ejemplo05

---
+++ Clase13/ejemplo05.js
@@ -0,0 +1,30 @@
+const dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves','Viernes','Sábado', 'Domingo', 'findelargo'];
+const numeros = [12,34,56,8,998];
+
+console.log(dias);
+console.log(numeros);
+
+console.log(dias.length);// cantidad
+
+console.log(dias[1]); //Martes
+
+const ultimo =dias[dias.length-1];
+
+console.log(ultimo)
+
+
+const frutas = ['palta', 'frutillas', 'manzana', 'banana'];
+
+console.log(frutas);
+
+console.log(frutas.length); //7
+
+console.log(frutas[frutas.length-1]); //7 -1 =6
+
+let seleccionado = 2;
+
+const seleccionado = 2;
+console.log(frutas[seleccionado]); //???
+
+seleccionado = 3;
+console.log(frutas[seleccionado]);
