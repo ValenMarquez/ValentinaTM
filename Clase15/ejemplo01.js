e15518baa7d7d493a833c167de9c08e46bd1cfcb
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-10-14 12:05:02 -0300

    ejemplo01.js

---
+++ Clase15/ejemplo01.js
@@ -0,0 +1,11 @@
+const parrafo = document.querySelector("p");
+
+const parrafos = document.querySelectorAll("p");
+
+console.log(parrafo);
+
+console.log(parrafos);
+
+const destacados = document.querySelectorAll(".destacados");//
+
+console.log(destacados)
