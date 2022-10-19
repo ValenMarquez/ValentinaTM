eaff959c56349de321db846fada4885e72feaecd
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-10-19 13:57:38 -0300

    ejemplo08.js

---
+++ Clase17/ejemplo08.js
@@ -0,0 +1,14 @@
+const nombre ='Flavia';
+const edad = 41;
+
+console.log(`hola! ${nombre}, asi que tienes ${edad} años.`);
+
+const edad1 = 18;
+const edad2 = 32;
+
+const mensaje1 = `La cuota para personas de ${edad1} es de ${edad1 >=21 ? 500 : 300}`;
+const mensaje2 = `La cuota para personas de ${edad2} es de ${edad2 >=21 ? 500 : 300}`;
+// operario
+
+console.log(mensaje1);
+console.log(mensaje2);
