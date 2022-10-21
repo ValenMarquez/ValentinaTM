ddcbd08fb48ec0feebd3e58a0cd18076fbfbb177
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-10-21 14:45:28 -0300

    ejemplo05.js

---
+++ Clase18/ejemplo05.js
@@ -0,0 +1,9 @@
+const colores = ['uno', 'dos', 'tres'];
+
+const [rojo, amarillo, verde] = colores;
+
+const nombre= 'Flavia'
+
+console.log(rojo);
+
+//la destructuración es una tecnica para declarar y extraer varias variables a la vez
