e42bac58f82e0835650c34c5f58256fddacb647a
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-10-21 14:45:51 -0300

    ejemplo06.js

---
+++ Clase18/ejemplo06.js
@@ -0,0 +1,7 @@
+const vocal =['a', 'b', 'c'];
+const vocalesCompletas = [...vocal, 'o', 'u'];
+
+console.log(vocal)
+console.log(vocalesCompletas)
+
+//sirve para obtener todas las propiedades de un objeto
