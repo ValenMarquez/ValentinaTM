c77df73c5e73c85e111553f9cfe2dd4425304828
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-11 10:48:00 -0300

    Footer.js

---
+++ clase22react/src/componentes/layouts/Footer.js
@@ -0,0 +1,12 @@
+import '../styles/footer.css'
+
+const Footer = (props) => {
+    return (
+        <footer>
+            Diseñado por Valentina Marquez -2022
+        </footer>
+
+    )
+}
+
+export default Footer;
