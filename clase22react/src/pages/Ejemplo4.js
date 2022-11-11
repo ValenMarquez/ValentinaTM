df001934d6170e2c82454f9ba300a1a0ed473c72
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-11 15:44:17 -0300

    Ejemplo4.js

---
+++ clase22react/src/pages/Ejemplo4.js
@@ -0,0 +1,19 @@
+import alumnos from '../data/alumnos.json';
+
+const Ejemplo4 = props => {
+    return (
+        <>
+            <h1>hola soy la pagina Ejemplo 4 JsON</h1>
+            <ul>
+                { alumnos.map(alumno => (
+                    <li>{alumno.nombre} {alumno.apellido} - edad: ({alumno.edad}) </li>
+                )
+
+                )}
+
+            </ul>
+        </>
+    )
+}
+
+export default Ejemplo4;
