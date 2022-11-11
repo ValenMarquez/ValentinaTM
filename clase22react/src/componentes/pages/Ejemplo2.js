13e9c3a4e7a90c1cdfa204a012ee3628cbd1fd9b
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-11 10:49:51 -0300

    Ejemplo2.js

---
+++ clase22react/src/componentes/pages/Ejemplo2.js
@@ -0,0 +1,14 @@
+import { Ejemplo1Estado, Ejemplo2Estado } from "../estado/Estado";
+import'../styles/estilos.css'
+
+
+const Ejemplo2 = props => {
+    return(
+        <div>
+            <h1>Ejemplos Estados</h1>
+            <Ejemplo1Estado/>
+            <hr/>
+        </div>
+    )}
+
+    export default Ejemplo2;
