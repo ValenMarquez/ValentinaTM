1f923e52082c56bd186cf9c39fdc20fb366e236d
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-11 10:36:47 -0300

    js03.js

---
+++ practica clase/js03.js
@@ -0,0 +1,28 @@
+const numeros = [15,25,1253,45,105,205]; //msyor wur no rdtr ni en ptincipio ni en final
+
+var mayor = 0;
+
+for(var i=0, i<numeros.length; i++){
+    if(numeros[i] > mayor){
+        mayor = numeros[i]
+    }}
+
+console.log(`El mayor de ${numeros} es ${mayor}`);
+
+
+
+const numeros = []; //msyor wur no rdtr ni en ptincipio ni en final
+
+
+for(let i=0, i<5; i++){
+    const num = parseInt(prompt('Ingrese un numero',''))
+numeros [i]=num;
+}
+
+console.log(numeros)
+
+if(numeros[i] > mayor){
+        mayor = numeros[i]
+    }}
+
+console.log(`El mayor de ${numeros} es ${mayor}`);
