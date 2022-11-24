2b912aa3d9c13b8969393b5a303623bf87213e23
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-24 10:56:35 -0300

    js03.js

--- practica clase/js03.js
+++ practica clase/js03.js
@@ -1,28 +1,29 @@
-const numeros = [15,25,1253,45,105,205]; //msyor wur no rdtr ni en ptincipio ni en final
+/*const numeros = [16,28,1426,48,156,208]; //mayor ni en principio ni en final!
 
-var mayor = 0;
+var mayor = 0; // 1426
 
-for(var i=0, i<numeros.length; i++){
+for(var i=0; i<numeros.length; i++) {
     if(numeros[i] > mayor){
         mayor = numeros[i]
-    }}
-
-console.log(`El mayor de ${numeros} es ${mayor}`);
-
-
-
-const numeros = []; //msyor wur no rdtr ni en ptincipio ni en final
-
-
-for(let i=0, i<5; i++){
-    const num = parseInt(prompt('Ingrese un numero',''))
-numeros [i]=num;
+    }
 }
 
+console.log(`El mayor de ${numeros} es ${mayor}`)*/
+
+
+const numeros = [];
+for (let i = 0; i < 5; i++) {
+    const num = parseInt(prompt('Ingrese un numero', ''))
+    numeros[i] = num;
+}
 console.log(numeros)
 
-if(numeros[i] > mayor){
+let mayor = 0;
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > mayor) {
         mayor = numeros[i]
-    }}
+    }
+}
 
-console.log(`El mayor de ${numeros} es ${mayor}`);
+document.write(`El mayor es ${mayor}`)
+
