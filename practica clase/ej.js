5a163b78e2c185544d45c7536a03e473348d24f5
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-24 10:55:54 -0300

    ej.js

--- practica clase/ej.js
+++ practica clase/ej.js
@@ -2,6 +2,7 @@ const texto = document.getElementById('texto');
 const contar = document.getElementById('contar');
 
 texto.addEventListener('keyup' , function(){
-    contar.innerText = texto.ariaValueMax.length; // contar los caracteres
+    contar.innerText = texto.ariaValueMax.length // contar los caracteres
 
 })
+
