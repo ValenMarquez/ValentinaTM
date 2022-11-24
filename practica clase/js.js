8fc852f86086295758e2e8992196660fd9768dde
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-24 10:56:16 -0300

    js.js

--- practica clase/js.js
+++ practica clase/js.js
@@ -1,21 +1,10 @@
+const nombre = prompt('Ingrese su nombre', '');
 const edad = parseInt(prompt('Ingrese su edad',''));
 
 if(Number.isNaN(edad)){
     document.write('Debes ingresar un número')
 } else if(edad >= 18){
-    document.write('Podes conducir')
+    document.write(`Hola ${nombre} , tienes la edad suficiente para conducir!`)
 } else{
-    document.write('No podes conducir')
+    document.write(`Hola ${nombre} ,No tienes la edad suficiente para conducir!`)
 }
-
-//function conducir() {
-//if(Number.isNaN(edad)){
-    document.write('La edad ingresada no es válida') 
-}else if (edad >= 18) {
-    document.write(`Hola ${nombre}, tienes edad suficiente para conducir.`)
-} else {
-    document.write(`Hola ${nombre}, no tienes edad suficiente para conducir.`)
-}
-}
-
-conducir();
