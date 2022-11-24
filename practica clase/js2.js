74f6421696e2fd881ef145dbbc66e69f87ca3e33
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-24 10:56:25 -0300

    js2.js

--- practica clase/js2.js
+++ practica clase/js2.js
@@ -1,4 +1,4 @@
-/* const metros = 3500000; 
+const metros = 3500000; 
 let transportes = '';
 
 if (metros > 0 && metros <=1000) {
@@ -13,29 +13,4 @@ if (metros > 0 && metros <=1000) {
     transportes = 'avion' 
 }
 
-console.log(`Hola! Para ${metros} metros te recomendamos ir en ${transportes}`); */
-
-//opcion 02
-
-const distancia = document.write(prompt('Ingrese en valor numerico los metros de su distancia a recorrer'))
-
-function transporteIndicado(distancia) {
-    if (distancia > 0 && metros <= 1000) {
-        document.write(`Para recorrer ${distancia} metros, la mejor forma es ir a pie.`)
-    }
-        else if (distancia > 1000 && metros <= 10000) {
-        document.write(`Para recorrer ${distancia} metros, la mejor forma es ir a bicicleta.`)
-    }
-        else if (distancia > 10000 && metros <= 30000) {
-        document.write(`Para recorrer ${distancia} metros, la mejor forma es ir a colectivo.`)
-    }
-        else if (distancia > 30000 && metros <= 100000) {
-        document.write(`Para recorrer ${distancia} metros, la mejor forma es ir a auto.`)
-    }
-        else {
-        document.write(`Para recorrer ${distancia} metros, la mejor forma es ir a avión.`)
-    }}
-
-
-
-transporteIndicado();
+console.log(`Hola! Para ${metros} metros te recomendamos ir en ${transportes}`); 
