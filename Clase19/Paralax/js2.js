2aa788c7f6377d7d0336a287ba94da118aaad423
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-25 08:20:45 -0300

    js2.js

--- Clase19/Paralax/js2.js
+++ Clase19/Paralax/js2.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
     $window - $(window);
 
     $('#intro').height($window.height());
-    $window.scrol(function(){
+    $window.scroll(function(){
         $('div.background').each(function(index,item){
             let $scroll = $(item);
             let yPos = -($window.scrollTop()/$scroll.data('speed'));
-            let coord= '50' + yPos + 'px';
-            $scroll.css ({backgroundPosition:coords});
+            let coord= '50% ' + yPos + 'px';
+            $scroll.css ({backgroundPosition: coords});
         })
     })
+})
