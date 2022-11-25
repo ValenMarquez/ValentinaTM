da3be51a72bb7b8f6a5ad0fe1a30a2bbac1ed669
Valentina Marquez <valentinamarquez.93@gmail.com>
2022-11-25 09:40:55 -0300

    ej 09

---
+++ Clase17/ejemplo09.js
@@ -0,0 +1,10 @@
+// definir estructuras de objetos o listas complejas
+
+const persona ={
+    nombre:'Valentina',
+    edad: 29,
+    trabajo: 'Diseñadora'
+}
+
+console.log(persona.nombre);
+console.log(persona.trabajo);
